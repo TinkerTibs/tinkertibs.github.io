a33acf1850b1723237f6d8fe1b3ef02f68cd5c6d
TinkerTibs <christinathibodeau@gmail.com>
2020-10-15 11:12:05 -0700

    formula update

--- js/poundsToKilograms.js
+++ js/poundsToKilograms.js
@@ -3,7 +3,7 @@ function poundsToKilograms() {
     let pounds = parseFloat(document.getElementById("pounds").value);
     //PROCESSING
     //conversion factor from pounds to kilograms
-    let kilograms = pounds * 0.453592
+    let kilograms = pounds / 2.20462262185
     //OUTPUT
     document.getElementById("output").textContent = pounds + " Pounds equals " + kilograms.toFixed(1) + " Kilograms";
 }
