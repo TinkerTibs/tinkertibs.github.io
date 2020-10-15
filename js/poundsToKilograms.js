b11cb23f10589fd40d5930faeece465ce29ee08e
TinkerTibs <christinathibodeau@gmail.com>
2020-10-15 10:13:36 -0700

    pounds to kilogram files

---
+++ js/poundsToKilograms.js
@@ -0,0 +1,9 @@
+function poundsToKilograms() {
+    //INPUT 
+    let pounds = parseFloat(document.getElementById("pounds").value);
+    //PROCESSING
+    //conversion factor from pounds to kilograms
+    let kilograms = pounds * 0.453592
+    //OUTPUT
+    document.getElementById("output").textContent = kilograms.toFixed(1) + " Kilograms";
+}
