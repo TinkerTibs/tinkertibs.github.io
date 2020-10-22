eaf949c43656b2b5618b74d09c4a8cd15ffa1e10
TinkerTibs <christinathibodeau@gmail.com>
2020-10-21 20:36:44 -0700

    updating comments

--- js/calculator.js
+++ js/calculator.js
@@ -1,10 +1,11 @@
 function checkAnswer() {
-    //INPUT
+    //INPUT 
     let number1 = parseFloat(document.getElementById("number1").value);
     let number2 = parseFloat(document.getElementById("number2").value);
     let operator = (document.getElementById("operator").value);
     let solution = parseFloat(document.getElementById("solution").value);
     //PROCESSING
+    //creating switch to diferinciate operators
 switch (operator) {
     case "+":
         answerKey = number1 + number2;
@@ -19,7 +20,7 @@ switch (operator) {
         answerKey = number1 / number2;
         break;
 }
-
+//if statement to verify if solution entered is correct
 if (answerKey == solution) {
     message = "Correct!"
 }   else {
