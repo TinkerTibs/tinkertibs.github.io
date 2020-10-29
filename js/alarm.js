33fb30a53c0f0028cc258d38b66d46790c01c1d4
TinkerTibs <christinathibodeau@gmail.com>
2020-10-29 14:59:48 -0700

    adding notes

--- js/alarm.js
+++ js/alarm.js
@@ -1,21 +1,25 @@
 function sleepCheck() {
     //INPUT
-        // all the imput is taken from the computer
-//    let now = new Date();
-//    let month = now.getMonth();
-//    let dayOfMonth = now.getDate();
-//    let dayOfWeek = now.getDay();
+    
+    // all the imput is taken from the computer
     let now = new Date();
     let month = now.getMonth();
     let dayOfMonth = now.getDate();
-    let dayOfWeek = 6;
-
+    let dayOfWeek = now.getDay();
 
     //PROCESSING
+
+    //establishing base message to get up
     let message = "Get Up!";
+
+    //if statement to change message to sleep in if the current days is any of the following...
+    // in order elimates: sunday, sautrday, New Year’s Day, U.S. Independence Day, & Christmas
     if (dayOfWeek == 0 || dayOfWeek == 6 || (month == 0 && dayOfMonth == 1) || (month == 6 && dayOfMonth == 4) || (month == 11 && dayOfMonth == 25) ) {
         message = "Sleep in";
     } 
+
     //OUTPUT
+
+    //outputs the message
     document.getElementById("output").textContent = message;
 }
