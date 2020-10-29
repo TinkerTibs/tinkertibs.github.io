22528d7bd731997431ac736f2a0b1aaf77e5dcc3
TinkerTibs <christinathibodeau@gmail.com>
2020-10-29 15:18:06 -0700

    update discount

--- js/discount.js
+++ js/discount.js
@@ -1,13 +1,20 @@
-function sleepCheck() {
+function calculateTotal() {
     //INPUT
-        // all the imput is taken from the computer
-    let now = new Date();
-    let month = now.getMonth();
-    let dayOfMonth = now.getDate();
-    let dayOfWeek = now.getDay();
+
+    //input user input and convert to float
+    let subtotal = parseFloat(document.getElementById("subtotal").value);
+
+    // input from the computer to determine day of the week
+//    let dayOfWeek = new Date().getDay();
+    let dayOfWeek = 2;
     //PROCESSING
 
-    message = new Date();
+    if (subtotal > 50 && (dayOfWeek == 2 || dayOfWeek == 3)) {
+        subtotal = subtotal - (subtotal * .1 )
+    }
+
     //OUTPUT
-    document.getElementById("output").textContent = message;
+
+    //outputs the message
+    document.getElementById("output").textContent = subtotal;
 }
