966acd546325e92817dfebd8a6452709e60dcef0
TinkerTibs <christinathibodeau@gmail.com>
2020-10-29 14:54:19 -0700

    test

--- js/alarm.js
+++ js/alarm.js
@@ -1,10 +1,16 @@
 function sleepCheck() {
     //INPUT
         // all the imput is taken from the computer
+//    let now = new Date();
+//    let month = now.getMonth();
+//    let dayOfMonth = now.getDate();
+//    let dayOfWeek = now.getDay();
     let now = new Date();
     let month = now.getMonth();
     let dayOfMonth = now.getDate();
-    let dayOfWeek = now.getDay();
+    let dayOfWeek = 6;
+
+
     //PROCESSING
     let message = "Get Up!";
     if (dayOfWeek == 0 || dayOfWeek == 6 || (month == 0 && dayOfMonth == 1) || (month == 6 && dayOfMonth == 4) || (month == 11 && dayOfMonth == 25) ) {
