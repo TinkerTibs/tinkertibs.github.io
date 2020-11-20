9b7dda2a1fbcaf8cb6a21d49effb8c6d1bf807e4
TinkerTibs <christinathibodeau@gmail.com>
2020-11-19 18:14:13 -0800

    invest

---
+++ js/invest.js
@@ -0,0 +1,26 @@
+function doFV() {
+    //INPUT 
+        //values are taken from user input
+        let invested = parseFloat(document.getElementById("invested").value);
+        let rate = parseFloat(document.getElementById("rate").value);
+        let numberYears = parseFloat(document.getElementById("numberYears").value);
+        let periodsPerYear = parseFloat(document.getElementById("periodsPerYear").value);
+    //PROCESSING
+
+        // calls second function
+        let result = computeFutureValue(invested, rate, numberYears, periodsPerYear);
+    //OUTPUT
+        //outputs result to user
+        document.getElementById("output").innerHTML = result;
+    }
+    
+    
+    
+    function computeFutureValue(principal, annualRate, years, periodsPerYear) {
+        futureValue = principal * Math.pow((1 + (annualRate/periodsPerYear)), (years * periodsPerYear))
+        return "$" + futureValue.toFixed(2);
+    }
+
+
+
+
