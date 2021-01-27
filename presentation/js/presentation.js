231280bc567671c06d6d62f23796cff33a1018a7
TinkerTibs <christinathibodeau@gmail.com>
2021-01-26 16:37:20 -0800

    work in progress

--- presentation/js/presentation.js
+++ presentation/js/presentation.js
@@ -2,3 +2,40 @@ var lastTampering = document.lastModified;
 document.getElementById("output").textContent = "Last Updated: " + lastTampering;
 var currentYear = new Date();
 document.getElementById("classYear").textContent = currentYear.getFullYear();
+
+function activateOne() {
+    document.getElementById('exampleOne').classList.add('exampleOneAnimation');
+}
+function activateTwo() {
+    document.getElementById('exampleTwo').classList.add('exampleTwoAnimation');
+}
+function activateThree() {
+    document.getElementById('exampleThree').classList.add('exampleThreeAnimation');
+}
+function activateFour() {
+    document.getElementById('exampleFour').classList.add('exampleFourAnimation');
+}
+function activateFive() {
+    document.getElementById('exampleFive').classList.add('exampleFiveAnimation');
+}
+function activateSix() {
+    document.getElementById('exampleSix').classList.add('exampleSixAnimation');
+}
+function activateSeven() {
+    document.getElementById('exampleSeven').classList.add('exampleSevenAnimation');
+}
+function activateEight() {
+    document.getElementById('exampleEight').classList.add('exampleEightAnimation');
+}
+function activateNine() {
+    document.getElementById('exampleNine').classList.add('exampleNineAnimation');
+}
+function activateTen() {
+    document.getElementById('exampleTen').classList.add('exampleTenAnimation');
+}
+function activateEleven() {
+    document.getElementById('exampleEleven').classList.add('exampleElevenAnimation');
+}
+function activateTwelve() {
+    document.getElementById('exampleTwelve').classList.add('exampleTwelveAnimation');
+}
