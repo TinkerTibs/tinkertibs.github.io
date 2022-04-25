6f3b51aabb04b87828a6c56fecebf69c689fb9ea
TinkerTibs <71239220+TinkerTibs@users.noreply.github.com>
2022-04-25 07:37:23 -0500

    week 2 notes

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@ document.getElementById("classYear").textContent = currentYear.getFullYear();
 
 
 const links = [
-   "<li>Week 1: <a href='/weekone/notes.html'>Notes </a></li>",
-   "<li>Week 1: <a href='/weekone/story_editor.html'>Story Editor </a></li>",
-   "<li>Week 2: <a href='/weektwo/notes.html'>Notes </a></li>"
+   "<li>Week 1: <a href='/week1/notes.html'>Notes </a></li>",
+   "<li>Week 1: <a href='/week1/story_editor.html'>Story Editor </a></li>",
+   "<li>Week 2: <a href='/week2/notes.html'>Notes </a></li>"
 ]
 document.getElementById("toclist").innerHTML = links.join("");
