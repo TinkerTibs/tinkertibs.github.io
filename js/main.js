524657f2b10f8b005d1469f349f4e0aec9734337
TinkerTibs <71239220+TinkerTibs@users.noreply.github.com>
2022-04-28 20:02:57 -0500

    group assignment

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ document.getElementById("classYear").textContent = currentYear.getFullYear();
 const links = [
    "<li>Week 1: <a href='/week1/notes.html'>Notes </a></li>",
    "<li>Week 1: <a href='/week1/story_editor.html'>Story Editor </a></li>",
-   "<li>Week 2: <a href='/week2/notes.html'>Notes </a></li>"
+   "<li>Week 2: <a href='/week2/notes.html'>Notes </a></li>",
+   "<li>Week 2: <a href='/week2/groupwork.html'>Group Assignment </a></li>"
 ]
 document.getElementById("toclist").innerHTML = links.join("");
