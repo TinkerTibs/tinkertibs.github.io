9376e3a48e594b69de5ff256e0b457fd9bae2631
TinkerTibs <71239220+TinkerTibs@users.noreply.github.com>
2022-05-06 10:30:33 -0500

    week 3 notes

--- js/main.js
+++ js/main.js
@@ -5,11 +5,12 @@ var currentYear = new Date();
 document.getElementById("classYear").textContent = currentYear.getFullYear();
 
 const links = [
-  "<li>Week 1: <a href='/week1/notes.html'>Notes </a></li>",
-  "<li>Week 1: <a href='/week1/story_editor.html'>Story Editor </a></li>",
-  "<li>Week 2: <a href='/week2/notes.html'>Notes </a></li>",
-  "<li>Week 2: <a href='/week2/groupwork.html'>Group Assignment </a></li>",
-  "<li>Week 3: <a href='/week3/notes.html'>Notes </a></li>",
-  "<li>Week 3: <a href='/week3/groupwork.html'>Group Assignment </a></li>",
+  "<li>Week 1: <a href='/week1/notes.html'>Notes</a></li>",
+  "<li>Week 1: <a href='/week1/story_editor.html'>Story Editor</a></li>",
+  "<li>Week 2: <a href='/week2/notes.html'>Notes</a></li>",
+  "<li>Week 2: <a href='/week2/groupwork.html'>Group Assignment</a></li>",
+  "<li>Week 3: <a href='/week3/notes.html'>Notes</a></li>",
+  "<li>Week 3: <a href='/week3/exersizes.html'>Examples & Exersizes</a></li>",
+  "<li>Week 3: <a href='/week3/groupwork.html'>Group Assignment</a></li>"
 ];
 document.getElementById("toclist").innerHTML = links.join("");
