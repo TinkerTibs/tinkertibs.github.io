f7bd68b68c154fe2c5ef3f603e168b5cd2b427f0
TinkerTibs <71239220+TinkerTibs@users.noreply.github.com>
2022-05-20 11:18:27 -0500

    group work

--- week5/js/hikemodel.js
+++ week5/js/hikemodel.js
@@ -1,49 +1,47 @@
-// normally the model would have more going on...retrieving the hikes from a database, adding hikes, editing hikes, filtering, etc. Our model will be very simple.  We could simply export the hikeList, but a better pattern would be to create a 'getter' function to do it instead. That way as our model changed...we could simply change the getter function and anything using it should be able to remain the same.
+// a simple model for the hikeList 
 const hikeList = [
   {
-  name: 'Bechler Falls',
-  imgSrc: 'falls.jpg',
-  imgAlt: 'Image of Bechler Falls',
-  distance: '3 miles',
-  difficulty: 'Easy',
-  description:
-      'Beautiful short hike along the Bechler river to Bechler Falls',
-  directions:
-      'Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead.'
+    name: "Bechler Falls",
+    imgSrc: "falls.jpg",
+    imgAlt: "Image of Bechler Falls",
+    distance: "3 miles",
+    difficulty: "Easy",
+    description:
+      "Beautiful short hike along the Bechler river to Bechler Falls",
+    directions:
+      "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead."
   },
   {
-  name: 'Teton Canyon',
-  imgSrc: 'falls.jpg',
-  imgAlt: 'Image of Bechler Falls',
-  distance: '3 miles',
-  difficulty: 'Easy',
-  description:
-      'Beautiful short hike along the Bechler river to Bechler Falls',
-  directions:
-      'Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Staline Raod for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead.'
+    name: "Teton Canyon",
+    imgSrc: "falls.jpg",
+    imgAlt: "Image of Bechler Falls",
+    distance: "3 miles",
+    difficulty: "Easy",
+    description: "Beautiful short (or long) hike through Teton Canyon.",
+    directions:
+      "Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Stateline Road for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead."
   },
   {
-  name: 'Denanda Falls',
-  imgSrc: 'falls.jpg',
-  imgAlt: 'Image of Bechler Falls',
-  distance: '3 miles',
-  difficulty: 'Easy',
-  description:
-      'Beautiful short hike along the Bechler river to Bechler Falls',
-  directions:
-      'Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to until you see the sign for Bechler Meadows on the left. Turn there. There is a parking area at the trailhead.'
+    name: "Denanda Falls",
+    imgSrc: "falls.jpg",
+    imgAlt: "Image of Bechler Falls",
+    distance: "7 miles",
+    difficulty: "Moderate",
+    description:
+      "Beautiful hike through Bechler meadows river to Denanda Falls",
+    directions:
+      "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to until you see the sign for Bechler Meadows on the left. Turn there. There is a parking area at the trailhead."
   }
 ];
-// Hike Model
-export default class HikeModel {
-constructor() {
-  // We need a constructor...but in this case it isn't doing much
+// a function that gets the list and then returns it
+class HikeModel {
+  getAllHikes() {
+    return hikeList;
+  }
+
+  getHikeByName(name) {
+    return hikeList.find((hike) => hike.name === name);
+  }
 }
-getAllHikes() {
-  // should return a list of all the hikes.
-}
-getHikeByName(hikeName) {
-  // filter the hikes for the record identified by hikeName and return it
-  return ;
-}
-}
+
+export default HikeModel;
--- week5/js/hikescontroller.js
+++ week5/js/hikescontroller.js
@@ -1,25 +1,41 @@
-import HikeModel from './HikeModel.js';
-import HikesView from './HikesView.js';
+//html file links to this main js page which links to the  HikeController js code
+//next we link to the HikeModel and HikesView js code
+//our HikeModel code sill store the list of hikes as the class HikeModel
+import HikeModel from "./HikeModel.js";
 
-// Hike controller
+import HikesView from "./HikesView.js";
+
+
 export default class HikesController {
   constructor(parentId) {
-    this.parentElement = document.getElementById(parentId); 
-    // this is how our controller will know about the model and view...we add them right into the class as members.
+    this.parentElement = document.getElementById(parentId);
     this.hikeModel = new HikeModel();
     this.hikesView = new HikesView(parentId);
   }
-  
   showHikeList() {
-    //  this will get called each time we need to display our full hike list. It should grab the list of hikes from the Model, and then send them to the view.
+    // the list of hikes will come from the model now...
+    const hikeList = this.hikeModel.getAllHikes();
+    // send the list of hikes and the element we would like those placed into to the view.
+    this.hikesView.renderHikeList(this.parentElement, hikeList);
+    // after the hikes have been rendered...add our listener
+    this.addHikeListener();
   }
-
   showOneHike(hikeName) {
-    // use this when you need to show just one hike...with full details
-    
+    const hike = this.hikeModel.getHikeByName(hikeName);
+    this.hikesView.renderOneHikeFull(this.parentElement, hike).ontouchend =
+      () => {
+        this.showHikeList();
+      };
   }
+  // adds listener after the list of hikes has been built
   addHikeListener() {
-    // for the stretch you will need to attach a listener to each of the listed hikes to watch for a touchend. 
-   
+    // loops through the children of our list and attach a listener to each, remember though that children is a nodeList...
+    //not an array. So in order to use something like a forEach we need to convert it to an array.
+    const childrenArray = Array.from(this.parentElement.children);
+    childrenArray.forEach((child) => {
+      child.addEventListener('touchend', (e) => {
+        this.showOneHike(e.currentTarget.dataset.name);
+      });
+    });
   }
 }
